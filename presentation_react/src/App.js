7c269fbe0cf8ebf530b435025dcb6e28aa8c7c75
Jean Eba <jeba760@gmail.com>
2023-06-14 22:40:59 +0100

    modif

--- presentation_react/src/App.js
+++ presentation_react/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       <p>Je suis l'application demo developper en react pour pour la présentation🙂 </p>
+       <p>Je suis l'application demo developper en react pour pour la présentation🙂 n </p>
       </header>
     </div>
   );
