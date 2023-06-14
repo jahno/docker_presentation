ab592d40382472c9a2178619c6045da1467a00f5
Jean Eba <jeba760@gmail.com>
2023-06-14 19:57:57 +0100

    first push reacrt app

---
+++ presentation_react/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       <p>Je suis l'application demo developper en react pour pour la présentation🙂 </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
