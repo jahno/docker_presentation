fae395096de0665562dd6f18422d90b45f88c6a0
Jean Eba <jeba760@gmail.com>
2023-06-14 22:51:50 +0100

    renew

--- presentation_react/src/App.js
+++ presentation_react/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       <p>Je   l'application demo developper en react pour pour la présentation🙂 n </p>
+       <p>Je suis l'application demo developper en react pour pour la présentation🙂 </p>
       </header>
     </div>
   );
